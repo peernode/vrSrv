23600fabbeaf625dac59b54fb74c4509cee36c03
xujy <xujy2013@126.com>
2016-03-23 13:33:22 +0800

    change default read timeout

--- main.go
+++ main.go
@@ -4,11 +4,18 @@ import (
 	"github.com/julienschmidt/httprouter"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 	router := httprouter.New()
 	initPathRouter(router)
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	s := &http.Server{
+		Addr:         ":8080",
+		Handler:      router,
+		ReadTimeout:  1000 * time.Second,
+		WriteTimeout: 500 * time.Second,
+	}
+	log.Fatal(s.ListenAndServe())
 }
